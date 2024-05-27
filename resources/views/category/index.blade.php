9294289a08e82dcd7b838c35e30e3f0c2138a947
ViniciusKarv <131013621+Vinicuskarv@users.noreply.github.com>
2024-05-27 22:41:04 +0100

    add-style

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -10,7 +10,7 @@
                 </div>
                 <ul class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a></li>
-                    <li class="breadcrumb-item"><<a href="{{ route('category') }}">Category</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('category') }}">Category</a></li>
                 </ul>
             </div>
         </div>
